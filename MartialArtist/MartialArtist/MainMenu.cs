1a49cf24cdafd69e320dba37032c636e3fee233e
phuc14789 <phuc14789@gmail.com>
2013-10-22 19:17:45 -0700

    Add MainMenu

---
+++ MartialArtist/MartialArtist/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace MartialArtist
+{
+    class MainMenu
+    {
+        private Texture2D menuBackground;
+
+        public void LoadContent(ContentManager Content)
+        {
+            menuBackground = Content.Load<Texture2D>("Images/Background");            
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(menuBackground, new Rectangle(0, 0, 800, 480), Color.White);     
+        }
+    }
+}
