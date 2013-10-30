204e3f06a42a93b5379603c58d35791fba7db3bf
phuc14789 <phuc14789@gmail.com>
2013-10-29 19:00:14 -0700

    Fix Jump

--- MartialArtist/MartialArtist/Player.cs
+++ MartialArtist/MartialArtist/Player.cs
@@ -17,9 +17,13 @@ namespace MartialArtist
         int health;
         int life;
         public Color[] textureData;
-        double vi, t = 0; 
-        double g = 5000;
-        int keyState = 0;
+
+        //Varible for control character
+        const float gravity = 50f;
+        Vector2 velocity;
+        float moveSpeed = 500f;
+        float jumpSpeed = 1000f;
+        bool jump = false;
 
         public Player(Texture2D enemy, Vector2 position, int health, int life, int currentFrame, int rows, int columns,float delay,float scale) : base(enemy ,position, currentFrame ,rows ,columns ,delay,scale )
         {
@@ -46,35 +50,34 @@ namespace MartialArtist
         {
             KeyboardState key = Keyboard.GetState();
 
-            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            //Move to left or right
+            if (key.IsKeyDown(Keys.D))
             {
-                keyState = 1; vi = -2020;
+                velocity.X = moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             }
+            else if (key.IsKeyDown(Keys.A))
+            {
+                velocity.X = -moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            else
+                velocity.X = 0;
 
-            if (keyState == 1)
+            //Jump
+            if (key.IsKeyDown(Keys.W) && jump)
             {
-                // the normal formula is: vi * t - g * t^2 / 2 and vi is positive, but here UP means decrease Y and DOWN means increase Y
-                // that's why it looks strange
-                _vt2_position.Y = (float)(vi * t + g * t * t / 2) + Global.screenHeight - _t_Image.Height;
-                t = t + gameTime.ElapsedGameTime.TotalSeconds; // calculate the time since the ball has left the ground
+                velocity.Y = -jumpSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                jump = false;
             }
+            if (!jump)
+                velocity.Y += gravity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            else
+                velocity.Y = 0;
 
-            if (_vt2_position.Y > Global.screenHeight - _t_Image.Height) // Don't allow the ball to go beyond the bottom side of the window
-            {
-                _vt2_position.Y = Global.screenHeight - _t_Image.Height;
-                keyState = 0;
-                t = 0;
-            }
-
-            if (Keyboard.GetState().IsKeyDown(Keys.A))
-            {
-                _vt2_position.X -= 5;
-            }
-
-            if (Keyboard.GetState().IsKeyDown(Keys.D))
-            {
-                _vt2_position.X += 5;
-            }
+            _vt2_position += velocity;
+            jump = _vt2_position.Y >= 200;
+            if (jump)
+                _vt2_position.Y = 200;
+        
         }
 
         public override void Draw(SpriteBatch spriteBatch)
